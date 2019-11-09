70c7557e55f5a42d0ba4244dc5859b6606d313f8
Chase Jensen <soccerchase@gmail.comm>
2019-11-09 05:11:02 -0700

    Actual initial commit

---
+++ bender1.rb
@@ -0,0 +1,12 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l, c = gets.split(" ").collect {|x| x.to_i}
+l.times do
+    row = gets.chomp
+end
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "answer"
