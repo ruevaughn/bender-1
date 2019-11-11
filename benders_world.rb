3a9d8fcc5d2675aff2be7b208cffe5c8c5e900c4
Chase Jensen <soccerchase@gmail.comm>
2019-11-11 00:21:18 -0700

    Update 2

--- benders_world.rb
+++ benders_world.rb
@@ -1,66 +1,105 @@
-module Map
-  OBSTACLES = {
-    'I' => 'INVERTER',
-    '@' => 'Bender',
-    '$' => 'SUICIDE_BOOTH',
-    '#' => 'OBSTACLE',
-    'X' => 'OBSTACLE',
-    'T' => 'Teleporter'
+module BendersWorld
+  OBJECTS = {
+    inverter: 'I',
+    bender: '@',
+    suicide_booth: '$',
+    boundary: '#',
+    obstacle: 'X',
+    teleporter: 'T'
   }.freeze
+  class Map
+    attr_accessor :lines
+    attr_accessor :columns
+    attr_accessor :bender
+
+    def init(*attrs)
+      @map = []
+      @bender_placed = false
+      @suicide_booth_placed = false
+      @teleporters_placed = 0
+    end
+
+    def generate_map
+      generate_map_size
+      draw_map
+    end
+
+    def self.random_object
+      [].tap do |available|
+         available << BendersWorld::Objects[:bender] unless @bender_placed
+         available << BendersWorld::Objects[:suicide_booth] unless @suicide_booth_placed
+         available << BendersWorld::Objects[:teleports] unless @teleporters_placed.two?
+         available << BendersWorld::Objects[:obstacle] unless @teleporters_placed.two?
+      end
+    end
 
 
-  def generate
-    get_size
+    private
 
+    def generate_map_size
+      # l, c = gets.split(" ").collect {|x| x.to_i}
+      # l.times do
+      #     row = gets.chomp
+      # end
+      puts 'How Many Lines (4-100)'
+      lines = gets.chomp
+      puts 'How Many Columns (4-100)'
+      columns = gets.chomp
+
+      if lines.present? && columns.present?
+        @lines = lines
+        @columns = columns
+      else
+        puts 'Try Again'
+        generate_map_size
+      end
+    end
+
+    def draw_map
+      horizontal_boundary
+      @lines.times do |line|
+        row = []
+        @columns.times do |column|
+          if column.zero?
+            row << Map::OBJECTS[:boundary]
+          elsif column == @columns.length
+            row << Map::OBJECTS[:boundary] 
+          else
+            row << Map::random_object 
+          end
+        end
+        @map << row
+      end
+      horizontal_boundary
+    end
+
+    def horizontal_boundary
+      row = []
+      @columns.times { row << MAP::OBJECTS[:boundary] }
+      @map << row
+    end
   end
 
-  def get_size
-    puts "This is Rick and Morty here generating Benders journey home.. *burp* just a few questions"
-    puts "How many far east - west do you want this drunken depressed slob to walk? (enter a number 4-100)"
-    gets lines
-    puts "What about north - south (enter a number 4-100)"
-    gets columns
+  class Bender
+    def init(*args)
+      @direction_facing = Bender::DIRECTION_PRIORITY[0]
+    end
+    DIRECTION_PRIORITY          = {south: 'S', east: 'E', north: 'N', west: 'W'}.freeze
+    DIRECTION_INVERTED_PRIORITY = {west: 'W', south: 'S', east: 'E', north: 'N'}.freeze
+
+    attr_accessor :direction_facing
+
+
+    def take_step
+    end
   end
+
 end
-module Journey
-  def start_walking_sideways
-    puts "You are bender. A misunderstood, innocent being. Never harmed a soul. You've had enough - you just want to get home."
-    puts "No, not to your house. Home. Where you came from. Where it all began. Drowning in sorrow, Bender, walking sideways"
-    puts "Begins his slow pity filled journey to the suicide booth. Unknowingly, something, whether his conscience, or this program"
-    puts "Something will (ideally) stop him. Let's see this Journey play out."
-  end
-end
 
-class Bender
-  DIRECTION_PRIORITY = 
-    'S' => 'SOUTH',
-    'E' => 'EAST',
-    'N' => 'North',
-    'W' => 'WEST'
-  }.freeze
+map = BendersWorld::Map.new
+map.generate_map
+puts map.map
 
-  DIRECTION_INVERTED_PRIORITY = {
-    'W' => 'WEST',
-    'S' => 'SOUTH',
-    'E' => 'EAST',
-    'N' => 'NORTH'
-    }.freeze
-
-  attr_accessor :direction_facing
-
-  def init(*args)
-    @direction_facing = Bender::DIRECTION_PRIORITY[:south]
-  end
-
-  def take_step
-  end
-
-end
-
-l, c = gets.split(" ").collect {|x| x.to_i}
-l.times do
-    row = gets.chomp
-end
 
 # Write an action using puts
 # To debug: STDERR.puts "Debug messages..."
